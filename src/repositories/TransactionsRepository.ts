e1a7ea1ea904c062f11364917a6e334f0c4191b2
joaodantas91 <joaostadantas@gmail.com>
2020-08-26 20:46:42 -0300

    first commit

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,16 @@
+import { response } from 'express';
 import Transaction from '../models/Transaction';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
@@ -14,15 +21,34 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const inComeTransactions = this.transactions
+      .filter(t => t.type === 'income')
+      .map(b => b.value)
+      .reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+    const outComeTransactions = this.transactions
+      .filter(t => t.type === 'outcome')
+      .map(b => b.value)
+      .reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+    const balance: Balance = {
+      income: inComeTransactions,
+      outcome: outComeTransactions,
+      total: inComeTransactions - outComeTransactions,
+    };
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,5 +1,12 @@
+import { response } from 'express';
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
+
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
@@ -8,8 +15,48 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: RequestDTO): Transaction {
+    /*
+    const transactionsIncome = this.transactionsRepository
+      .all()
+      .filter(t => t.type === 'income');
+    const transactionsIncomeValues = transactionsIncome
+      .map(t => t.value)
+      .reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+    const transactionsOutcome = this.transactionsRepository
+      .all()
+      .filter(t => t.type === 'outcome');
+    const transactionsOutcomeValues = transactionsOutcome
+      .map(b => b.value)
+      .reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+    */
+
+    /*
+    if (type === 'outcome') {
+      if (value > transactionsIncomeValues) {
+        throw Error;
+      }
+    }
+    */
+
+    const total = this.transactionsRepository.getBalance();
+
+    console.log(total.total);
+
+    if (type === 'outcome') {
+      if (value > total.total) {
+        throw Error('asaaaa');
+      }
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
